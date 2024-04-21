042ab646006847ca0191d17f5d0dbc8bb7cbac63
Tyler Smith <tylerlwsmith@gmail.com>
2024-04-20 19:08:37 -0700

    Add tpope's fugitive plugin

--- init.lua
+++ init.lua
@@ -231,7 +231,8 @@ require('lazy').setup({
   -- NOTE: Plugins can also be added by using a table,
   -- with the first argument being the link and the following
   -- keys can be used to configure plugin behavior/loading/etc.
-  --
+  'https://github.com/tpope/vim-fugitive',
+
   -- Use `opts = {}` to force a plugin to be loaded.
   --
   --  This is equivalent to:
